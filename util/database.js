fc7cba75364377c0e3d6000b30c7fa4098ef7486
vinayarva <vinaykumararva13@gmail.com>
2024-10-02 16:44:18 +0530

    trying to use sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "nodejs",
-    password: "vinayarva1306",
-    port:2000,
+const sequelize = new Sequelize('nodejs', 'root', 'vinayarva1306', '3000' , {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
